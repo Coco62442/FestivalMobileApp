bc28298028721816e9506ee58222ce24bd0888e7
MathildeTrb <mathilde.tribot@etu.umontpellier.fr>
2023-03-16 10:16:42 +0100

    first commit

---
+++ FestivalMobileApp/FestivalMobileApp/FestivalMobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FestivalMobileAppApp.swift
+//  FestivalMobileApp
+//
+//  Created by etud on 16/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FestivalMobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
